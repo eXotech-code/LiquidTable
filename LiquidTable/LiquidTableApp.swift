22db89cda8b98cb7b160e6bdfe1d8b689b283c31
Jakub Piskiewicz <jakub@piskiewicz.org>
2023-11-11 15:07:26 +0100

    Initial Commit

---
+++ LiquidTable/LiquidTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidTableApp.swift
+//  LiquidTable
+//
+//  Created by Jakub Piskiewicz on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiquidTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
